261761cdc7a9a98a6ec4f240d554121ee8375676
Gregório Patriota <gregpatriota@Gregorios-MacBook-Air.local>
2024-08-25 17:22:06 -0300

    Five exercises related to the second module class

---
+++ lab02/Ex2.cs
@@ -0,0 +1,107 @@
+﻿//using System;
+//using System.Diagnostics;
+//using System.Threading.Tasks;
+
+
+///**
+// * 
+// * .      /)─―ヘ/)
+// *  　 ＿／　　　　＼
+// *   ／　　　　●　　　●
+// *  ｜　　　　　　　▼ |
+// *  ｜　　　　　　　 ノ 　
+// *__ U￣U￣￣￣U￣U
+// * */
+//class Program
+//{
+//    /// <summary>
+//    /// Calcula a média dos valores de um dado array de valores
+//    /// </summary>
+//    private double Media(double[] sequence)
+//    {
+//        double total = 0;
+//        for (int i = 0; i < sequence.Length; i++)
+//        {
+//            total += sequence[i];
+//        }
+
+//        return total / sequence.Length;
+//    }
+
+//    /// <summary>
+//    /// Calcula a mediana dos valores de um dado array de valores
+//    /// </summary>
+//    private double Mediana(double[] sequence)
+//    {
+//        Array.Sort(sequence);
+
+//        return (sequence.Length % 2 == 1) ?
+//            sequence[sequence.Length / 2] :
+//            (sequence[sequence.Length / 2 - 1] + sequence[sequence.Length / 2]) / 2;
+//    }
+
+//    /// <summary>
+//    /// Calcula a variancia dos valores de um dado array de valores e a média deste array
+//    /// </summary>
+//    private double Variancia(double[] sequence, double media)
+//    {
+//        double totalDosQuadrados = 0;
+//        for (int i = 0; i < sequence.Length; i++)
+//        {
+//            totalDosQuadrados += Math.Pow(sequence[i] - media, 2);
+//        }
+//        return totalDosQuadrados / sequence.Length;
+//    }
+
+//    /// <summary>
+//    /// Calcula o desvio padrão dado a variância de um array de valores
+//    /// </summary>
+//    private double DesvioPadrao(double variancia)
+//    {
+//        return Math.Sqrt(variancia);
+//    }
+
+
+//    static void Main(string[] args)
+//    {
+//        // Ler N da entrada
+//        int N = int.Parse(Console.ReadLine());
+
+//        double[] sequence = new double[N];
+
+//        // Inicializar o array com reais aleatórios entre 0 e 100
+//        Random rand = new Random();
+//        for (int i = 0; i < N; i++)
+//        {
+//            sequence[i] = rand.NextDouble() * 100;
+//        }
+
+//        // Continue a Implementação
+//        // ...
+//        Stopwatch timer = new();
+//        Program p = new();
+
+//        timer.Start();
+//        var s_media = p.Media(sequence);
+//        var s_mediana = p.Mediana(sequence);
+//        var s_variancia = p.Variancia(sequence, s_media);
+//        var s_desvio = p.DesvioPadrao(s_variancia);
+//        timer.Stop();
+//        var s_time = timer.ElapsedMilliseconds;
+
+//        timer = new();
+//        timer.Start();
+//        var media = Task.Run(() => p.Media(sequence)); //Executa o calculo de média em uma thread separada, não há dependencia de valores anteriores
+//        var mediana = Task.Run(() => p.Mediana(sequence));//Executa o calculo da mediana em uma thread separada, não há dependencia de valores anteriores
+//        var variancia = Task.Run(() => p.Variancia(sequence, media.Result)); //Executa o calculo de variância em uma thread separada, mas bloqueará a execução até que o resultado de média esteja pronto
+//        var desvio = Task.Run(() => p.DesvioPadrao(variancia.Result)); //Executa o calculo de desvio padrão em uma thread separada, mas bloqueará a execução até que o resultado de variância esteja pronto
+//        desvio.Wait(); //Aguarda o resultado da thread de Desvio Padrão esteja pronto
+//        timer.Stop();
+
+//        var p_time = timer.ElapsedMilliseconds;
+
+//        Console.WriteLine($"Media = {media.Result}\nMediana = {mediana.Result}\nVariancia = {variancia.Result}\nDesvio Padrao = {desvio.Result}");
+//        Console.WriteLine($"Tempo Total = {s_time}/{p_time}ms");
+
+//    }
+//}
---
+++ lab02/Ex5.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+internal record PokemonDTO
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("name")]
+    public string Nome { get; set; }
+    [JsonPropertyName("types")]
+    public IList<PokemonTiposDTO> Tipos { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Nome}, {Tipos.Select(el => el.ToString()).Aggregate((acc, x) => $"{acc}, {x}")}";
+    }
+}
+
+internal record PokemonTiposDTO
+{
+    [JsonPropertyName("type")]
+    public PokemonTipoDTO Tipo { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Tipo}";
+    }
+}
+
+internal record PokemonTipoDTO
+{
+    [JsonPropertyName("name")]
+    public string Nome { get; set; }
+
+    public override string ToString()
+    {
+        return Nome;
+    }
+}
+
+
+class Program
+{
+    private void EscreveNovoArquivo(PokemonDTO pokemon)
+    {
+        string fileName = "pokedex.txt";
+
+        using (StreamWriter stream = new(fileName))
+        {
+            stream.WriteLine(pokemon.ToString());
+        }
+    }
+
+    private void EscreveNovoArquivo(IList<PokemonDTO> pokemons)
+    {
+        string fileName = "pokedex.txt";
+
+        using (StreamWriter stream = new(fileName))
+        {
+            foreach (PokemonDTO pokemon in pokemons)
+            {
+                stream.WriteLine(pokemon.ToString());
+            }
+        }
+    }
+
+    private void EscreveNoArquivo(PokemonDTO pokemon)
+    {
+        string fileName = "pokedex.txt";
+
+        using (StreamWriter stream = File.AppendText(fileName))
+        {
+            stream.WriteLine(pokemon.ToString());
+        }
+    }
+
+    private void EscreveNoArquivo(IList<PokemonDTO> pokemons)
+    {
+        string fileName = "pokedex.txt";
+
+        using (StreamWriter stream = File.AppendText(fileName))
+        {
+            foreach (PokemonDTO pokemon in pokemons)
+            {
+                stream.WriteLine(pokemon.ToString());
+            }
+        }
+    }
+
+    private PokemonDTO GetPokemon(int id)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://pokeapi.co/api/v2/pokemon/{id}/");
+        request.Method = "GET";
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        {
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = reader.ReadToEnd();
+                return JsonSerializer.Deserialize<PokemonDTO>(result);
+            }
+        }
+    }
+
+    private async Task<PokemonDTO> GetPokemonAsync(int id)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+            HttpResponseMessage response = await client.GetAsync($"pokemon/{id}/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<PokemonDTO>(result);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int P = input.Length;
+        int[] pokemonIds = new int[P];
+
+        for (int i = 0; i < P; i++)
+        {
+            pokemonIds[i] = int.Parse(input[i]);
+        }
+
+        // Continue a Implementação
+        // ...
+        Program p = new();
+
+        //var c = p.GetPokemon(pokemonIds[0]);
+        var c = p.GetPokemonAsync(pokemonIds[0]).Result;
+
+        Console.WriteLine(c);
+    }
+}
