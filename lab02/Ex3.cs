0136d94d933aba66219940e65df09eac78738aef
Gregório Patriota <gregpatriota@Gregorios-MacBook-Air.local>
2024-08-26 17:39:50 -0300

    LAB02: uncommenting source code from exercise 02, this lesson is complete

--- lab02/Ex3.cs
+++ lab02/Ex3.cs
@@ -1,62 +1,62 @@
-﻿//using System;
-//using System.Diagnostics;
-//using System.IO;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 
-//class Program
-//{
-//    /// <summary>
-//    /// Usa o LINQ para fazer a contagem de palavras em um texto com CaseInsensitive
-//    /// </summary>
-//    private int ContaPalavras(string palavraChave, string texto)
-//    {
-//        return texto.Split(' ').Count(palavra => palavra.Equals(palavraChave, StringComparison.OrdinalIgnoreCase));
-//    }
+class Program
+{
+   /// <summary>
+   /// Usa o LINQ para fazer a contagem de palavras em um texto com CaseInsensitive
+   /// </summary>
+   private int ContaPalavras(string palavraChave, string texto)
+   {
+       return texto.Split(' ').Count(palavra => palavra.Equals(palavraChave, StringComparison.OrdinalIgnoreCase));
+   }
 
 
-//    static void Main(string[] args)
-//    {
-//        string fileName = "..\\..\\..\\" + Console.ReadLine();
-//        int N = int.Parse(Console.ReadLine());
+   static void Main(string[] args)
+   {
+       string fileName = Console.ReadLine();
+       int N = int.Parse(Console.ReadLine());
 
-//        string[] words = new string[N];
+       string[] words = new string[N];
 
-//        for (int i = 0; i < N; i++)
-//        {
-//            words[i] = Console.ReadLine();
-//        }
+       for (int i = 0; i < N; i++)
+       {
+           words[i] = Console.ReadLine();
+       }
 
-//        string text;
-//        try
-//        {
-//            text = File.ReadAllText(fileName);
-//        }
-//        catch (Exception e)
-//        {
-//            Console.WriteLine("Erro ao ler o arquivo: ", e.Message);
-//            return;
-//        }
+       string text;
+       try
+       {
+           text = File.ReadAllText(fileName);
+       }
+       catch (Exception e)
+       {
+           Console.WriteLine("Erro ao ler o arquivo: ", e.Message);
+           return;
+       }
 
-//        // Continue a Implementação
-//        // ...
-//        Program p = new();
-//        Stopwatch timer = new();
-//        IList<Task> tasks = new List<Task>();
+       // Continue a Implementação
+       // ...
+       Program p = new();
+       Stopwatch timer = new();
+       IList<Task> tasks = new List<Task>();
 
-//        timer.Start();
-//        foreach (string word in words) //Cada contagem de palavra irá ser executada em uma Task independente
-//        {
-//            var t = Task.Run(() =>
-//            {
-//                var total = p.ContaPalavras(word, text);
-//                Console.WriteLine($"{word} ({total})");
-//            });
-//            tasks.Add(t);
-//        }
-//        Task.WaitAll(tasks.ToArray()); //Irá esperar todas as Tasks terminarem a execução para seguir
+       timer.Start();
+       foreach (string word in words) //Cada contagem de palavra irá ser executada em uma Task independente
+       {
+           var t = Task.Run(() =>
+           {
+               var total = p.ContaPalavras(word, text);
+               Console.WriteLine($"{word} ({total})");
+           });
+           tasks.Add(t);
+       }
+       Task.WaitAll(tasks.ToArray()); //Irá esperar todas as Tasks terminarem a execução para seguir
 
-//        timer.Stop();
-//        Console.WriteLine($"Em {timer.ElapsedMilliseconds}ms");
-//    }
-//}
+       timer.Stop();
+       Console.WriteLine($"Em {timer.ElapsedMilliseconds}ms");
+   }
+}
