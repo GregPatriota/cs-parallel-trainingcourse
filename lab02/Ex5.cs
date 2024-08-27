6481a7e89208b194df773c76fb32f265638b64fe
Gregorio Patriota <gpc.cin@gmail.com>
2024-08-27 06:52:52 -0300

    LAB02: adding comment header to refer the documentation

--- lab02/Ex5.cs
+++ lab02/Ex5.cs
@@ -8,6 +8,16 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
+/**
+* //////////////////////////////////
+* // As avaliações e observações //
+* // das implementações estão no //
+* //   documento em anexado      //
+* /////////////////////////////////
+* .       (\__/)  ||
+* .       (•ㅅ•)  ||
+* .       ( 　 づ || 
+**/
 internal record PokemonDTO
 {
     [JsonPropertyName("id")]
